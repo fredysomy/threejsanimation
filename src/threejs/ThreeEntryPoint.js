c91f47ab2298adc9435e76c0a11a0ef81167e58f
fredysomy <fredysomy@gmail.com>
2021-02-12 07:45:25 +0000

    added models from remote repo

--- src/threejs/ThreeEntryPoint.js
+++ src/threejs/ThreeEntryPoint.js
@@ -1,13 +1,13 @@
 import * as THREE from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import {MTLLoader} from "three/examples/jsm/loaders/MTLLoader.js"
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
 export default function ThreeEntryPoint(sceneRef) {
   const scene = new THREE.Scene();
   scene.background = new THREE.Color("red");
   const color = 0xffffff;
   const intensity = 1;
-  
+
   scene.background = new THREE.Color("white");
   const camera = new THREE.PerspectiveCamera(
     75,
@@ -15,76 +15,41 @@ export default function ThreeEntryPoint(sceneRef) {
     0.1,
     10000
   );
-  camera.position.set(0,0,10);
+  camera.position.set(0, 50, 300);
 
   const loader = new THREE.TextureLoader();
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   sceneRef.appendChild(renderer.domElement);
-const spotLight = new THREE.SpotLight( 0xffffff );
-spotLight.position.set( 0,10,100 );
-scene.add( spotLight );
-
-const spotLight1 = new THREE.SpotLight( 0xffffff );
-spotLight1.position.set( 0,-20,100 );
-scene.add( spotLight1 );
-
-const spotLight2 = new THREE.SpotLight( 0xffffff );
-spotLight2.position.set( 10,0,100 );
-scene.add( spotLight2 );
-
-const spotLight3 = new THREE.SpotLight( 0xffffff );
-spotLight3.position.set( -10,-5,100 );
-scene.add( spotLight3 );
-
-const width = 10;
-const height = 10;
-const intensity2= 1;
-const rectLight = new THREE.RectAreaLight( 0xffffff, intensity2,  width, height );
-rectLight.position.set( 0, 0, 100 );
-rectLight.lookAt( 0, 0, 0 );
-scene.add( rectLight )
-const light0 = new THREE.PointLight( 0xff0000, 1, 10000 );
-light0.position.set( 5, 5, 5 );
-scene.add( light0 );
-
-const light1 = new THREE.PointLight( 0xff0000, 1, 10000 );
-light1.position.set( 0, 10, 0 );
-scene.add( light1 );
-
-const light = new THREE.PointLight( 0xff0000, 1.2, 20000 );
-light.position.set( 0, -10, 0 );
-scene.add( light );
+  const spotLight = new THREE.SpotLight(0xffffff);
+  spotLight.position.set(0, 10, 100);
+  scene.add(spotLight);
 
   let controls = new OrbitControls(camera, sceneRef);
   controls.target.set(0, 0, 0);
   controls.rotateSpeed = 0.5;
   controls.update();
-  
-  var mtlLoader = new MTLLoader();
-mtlLoader.setBaseUrl( 'https://raw.githubusercontent.com/fredysomy/threejsanimation/master/src/models/gilr/' );
-mtlLoader.setPath( 'https://raw.githubusercontent.com/fredysomy/threejsanimation/master/src/models/gilr/' );
-var url = "Project%20Name.mtl";
-mtlLoader.load( url, function( materials ) {
-
-    materials.preload();
-
-    var objLoader = new OBJLoader();
-    objLoader.setMaterials( materials );
-    objLoader.setPath( 'https://raw.githubusercontent.com/fredysomy/threejsanimation/master/src/models/gilr/' );
-    objLoader.load('Project%20Name.obj', function ( object ) {
-
-        object.position.y = - 95;
-        scene.add( object );
-
-    });
-
-});
+  const mtlLoader = new MTLLoader();
+  mtlLoader.load(
+    "https://raw.githubusercontent.com/fredysomy/3dmodels/main/torusball/torusball.mtl",
+    (mtl1) => {
+      mtl1.preload();
+      const objLoader = new OBJLoader();
+      objLoader.setMaterials(mtl1);
+      objLoader.load(
+        "https://raw.githubusercontent.com/fredysomy/3dmodels/main/torusball/torusball.obj",
+        (root) => {
+          scene.add(root);
+          root.position.set(0, -4, 0);
+        }
+      );
+    }
+  );
 
   const animate = function () {
     requestAnimationFrame(animate);
-  
+
     renderer.render(scene, camera);
   };
 
