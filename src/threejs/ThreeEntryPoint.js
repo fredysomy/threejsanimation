4d3493ce823055ee31eb49a26095f86190819283
Fredy Somy <fredysomy@gmail.com>
2021-02-13 15:58:35 +0530

    Update ThreeEntryPoint.js

--- src/threejs/ThreeEntryPoint.js
+++ src/threejs/ThreeEntryPoint.js
@@ -34,13 +34,13 @@ scene.add( directionalLight );
   controls.update();
   const mtlLoader = new MTLLoader();
   mtlLoader.load(
-    "https://raw.githubusercontent.com/fredysomy/3dmodels/main/torusball/torusball.mtl",
+    "https://raw.githubusercontent.com/fredysomy/3dmodels/main/house/house.mtl",
     (mtl1) => {
       mtl1.preload();
       const objLoader = new OBJLoader();
       objLoader.setMaterials(mtl1);
       objLoader.load(
-        "https://raw.githubusercontent.com/fredysomy/3dmodels/main/torusball/torusball.obj",
+        "https://raw.githubusercontent.com/fredysomy/3dmodels/main/house/house.obj",
         (root) => {
           scene.add(root);
           root.position.set(0, -4, 0);
